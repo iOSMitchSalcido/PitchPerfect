f162c37bf6c80b2b5fc38a4a689ea0bf46af4dcb
Mitch Salcido <onlinetrainingemail@gmail.com>
2016-05-02 10:06:35 -0700

    functional. Deleted PlaySounds extension

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -25,45 +25,164 @@ class PlaySoundsViewController: UIViewController {
     @IBOutlet weak var reverbButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     
+    // ref to AVAudio classes used to play audio
     var recordedAudioURL: NSURL!
     var audioFile: AVAudioFile!
     var audioEngine: AVAudioEngine!
     var audioPlayerNode: AVAudioPlayerNode!
-    var stopTimer: NSTimer!
     
-    enum ButtonType: Int {case Slow = 0, Fast, Chipmunk, Vader, Echo, Reverb}
-
+    // enum for sounds. Also used to mask which effect button was pressed
+    enum SoundEffect: Int {
+        case Snail = 0, Rabbit, Chipmunk, Vader, Echo, Reverb
+    }
+    
+    // enum for play state. Used when setting state of buttons in view
+    enum PlayState {
+        case Ready, NotReady, Broken
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupAudio()
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        configureUI(.NotPlaying)
-    }
-    
-    @IBAction func playSoundForButton(sender: UIButton) {
-        
-        switch(ButtonType(rawValue: sender.tag)!) {
-        case .Slow:
-            playSound(rate: 0.5)
-        case .Fast:
-            playSound(rate: 1.5)
-        case .Chipmunk:
-            playSound(pitch: 1000)
-        case .Vader:
-            playSound(pitch: -1000)
-        case .Echo:
-            playSound(echo: true)
-        case .Reverb:
-            playSound(reverb: true)
+        // test for valid audio file
+        do {
+            audioFile = try AVAudioFile(forReading: recordedAudioURL)
+            configurePlayState(.Ready)
+        }
+        catch {
+            configurePlayState(.Broken)
         }
     }
     
+    // function to play audio
+    func playAudio(effect: SoundEffect) {
+        
+        // place buttons in not ready state
+        configurePlayState(.NotReady)
+        
+        // configure AVAudio classes
+        audioPlayerNode = AVAudioPlayerNode()
+        audioEngine = AVAudioEngine()
+        audioPlayerNode.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        // declare AVAudioNode. Will be set in switch statement
+        var audioNode: AVAudioNode!
+        
+        // create sound effect
+        switch effect {
+        case .Snail:
+            let node = AVAudioUnitTimePitch()
+            node.rate = 0.5
+            audioNode = node
+        case .Rabbit:
+            let node = AVAudioUnitTimePitch()
+            node.rate = 1.5
+            audioNode = node
+        case .Chipmunk:
+            let node = AVAudioUnitTimePitch()
+            node.pitch = 1000
+            audioNode = node
+        case .Vader:
+            let node = AVAudioUnitTimePitch()
+            node.pitch = -1000
+            audioNode = node
+        case .Echo:
+            let node = AVAudioUnitDistortion()
+            node.loadFactoryPreset(.MultiEcho1)
+            audioNode = node
+        case .Reverb:
+            let node = AVAudioUnitReverb()
+            node.loadFactoryPreset(.Cathedral)
+            node.wetDryMix = 50
+            audioNode = node
+        }
+        
+        // configure engine and player, attach/connect nodes
+        audioEngine.attachNode(audioNode)
+        audioEngine.connect(audioPlayerNode, to: audioNode, format: nil)
+        audioEngine.connect(audioNode, to: audioEngine.outputNode, format: audioFile.processingFormat)
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil) { () -> Void in
+            
+            // place UI update on main thread
+            NSOperationQueue.mainQueue().addOperationWithBlock() {
+                self.configurePlayState(.Ready)
+            }
+        }
+        
+        // play audio
+        do {
+            try audioEngine.start()
+            audioPlayerNode.play()
+        }
+        catch {
+            print("unable to play audio")
+        }
+    }
+    
+    // action for sound effect buttons
+    @IBAction func playSoundForButton(sender: UIButton) {
+        
+        // use button tag to create a SoundEffect enum
+        switch(SoundEffect(rawValue: sender.tag)!) {
+        case .Snail:
+            playAudio(.Snail)
+        case .Rabbit:
+            playAudio(.Rabbit)
+        case .Chipmunk:
+            playAudio(.Chipmunk)
+        case .Vader:
+            playAudio(.Vader)
+        case .Echo:
+            playAudio(.Echo)
+        case .Reverb:
+            playAudio(.Reverb)
+        }
+    }
+    
+    // action for stop button
     @IBAction func stopButtonPressed(sender: AnyObject) {
-        stopAudio()
+        
+        // verify audioPlayerNode, stop
+        if let audioPlayerNode = audioPlayerNode {
+            audioPlayerNode.stop()
+        }
+        
+        // verify audioEngine, stop and reset
+        if let audioEngine = audioEngine {
+            audioEngine.stop()
+            audioEngine.reset()
+        }
+        
+        // update UI to ready state
+        configurePlayState(.Ready)
+    }
+    
+    // helper function to enable/disable buttons in view
+    func configurePlayState(state: PlayState) {
+        
+        switch state {
+        case .Ready:
+            snailButton.enabled = true
+            chipmunkButton.enabled = true
+            rabbitButton.enabled = true
+            vaderButton.enabled = true
+            echoButton.enabled = true
+            reverbButton.enabled = true
+            stopButton.enabled = false
+        case .NotReady:
+            snailButton.enabled = false
+            chipmunkButton.enabled = false
+            rabbitButton.enabled = false
+            vaderButton.enabled = false
+            echoButton.enabled = false
+            reverbButton.enabled = false
+            stopButton.enabled = true
+        case .Broken:
+            configurePlayState(.NotReady)
+            stopButton.enabled = false
+        }
     }
 }
